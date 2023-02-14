6c4c49874a49ec1a21b933af43a2fd1007b3de61
CyCJIuK47 <alexander.suslynets@gmail.com>
2023-02-14 11:59:44 +0200

    Complete Task7
    Add data loading animation

--- Java/Task7/ui-base-app/src/pages/BookList/containers/BookList.jsx
+++ Java/Task7/ui-base-app/src/pages/BookList/containers/BookList.jsx
@@ -5,6 +5,7 @@ import useChangePage from 'hooks/useChangePage';
 import useLocationSearch from 'hooks/useLocationSearch';
 import BookCard from 'components/BookCard';
 import Button from 'components/Button';
+import Typography from 'components/Typography';
 import { useIntl } from 'react-intl';
 import Grid from 'components/Grid';
 import { fetchBooks, fetchBookDelete, removeBookFromPage, resetBookDelete, resetBooks } from 'app/actions/book';
@@ -12,6 +13,7 @@ import { fetchAuthors, resetAuthors } from 'app/actions/author';
 import * as PAGES from 'constants/pages';
 import { useSnackbar } from 'notistack';
 import useAccessValidate from 'hooks/useAccessValidate';
+import CircularProgress from 'components/CircularProgress';
 
 
 const BookList = ({ authorities }) => {
@@ -63,9 +65,6 @@ const BookList = ({ authorities }) => {
         }
     }, [authors.list]);
 
-
-
-
     useEffect(() => {
         if (deleteBook.isDeleted) {
             enqueueSnackbar(formatMessage({
@@ -83,56 +82,83 @@ const BookList = ({ authorities }) => {
         }
     }, [deleteBook.isFailed]);
 
-
     const { formatMessage } = useIntl();
 
-    return (
-    <>
-        { Object.keys(authorsMap).length !==0 &&
-        (<Grid container justify="space-around" spacing={2} >
-            <Grid container item xs={12} spacing={2}>
 
-                <Grid item xs={12}>
-                    <Button
-                    fullWidth
-                    color="secondary"
-                    variant="outlined"
-                    onClick={() => changePage({ locationSearch: locationSearch,
-                                                path: `/${PAGES.INITIAL}` })}
-                    >
-                        {formatMessage({
-                              id: 'back.to.main.page',
-                        })}
-                    </Button>
-                </Grid>
-                <Grid item xs={12}>
-                    {canEditList && (<Button
-                    variant="outlined"
-                    fullWidth
-                    color="primary"
-                    onClick={() => changePage({ locationSearch: locationSearch,
-                                                path: `/${PAGES.BOOK_EDIT}` })}
-                    > {formatMessage({
-                          id: 'book.create',
-                      })} </Button>)}
-                </Grid>
+    return (
+    <Grid container spacing={2} xs={12}>
+        <Grid item container xs={12} spacing={2}>
+            <Grid item xs={12}>
+                <Button
+                fullWidth
+                color="secondary"
+                variant="outlined"
+                onClick={() => changePage({ locationSearch: locationSearch,
+                                            path: `/${PAGES.INITIAL}` })}
+                >
+                    {formatMessage({
+                          id: 'back.to.main.page',
+                    })}
+                </Button>
             </Grid>
-            {
-                books.list.map((item)=>{
-                    const author = authorsMap[Number(item.authorId)];
-                    return (
-                        <Grid item >
-                            <BookCard book={item} author={author}
-                                onEdit={() => changePage({ path: `/${PAGES.BOOK_EDIT}/${item.id}` })}
-                                onDelete={ () => dispatch(removeBookFromPage({id: item.id}))}
-                                canEditCard={canEditList}
-                            />
-                        </Grid>
-                    );
-                })
-            }
-        </Grid>)}
-    </>
+            <Grid item xs={12}>
+                {canEditList && (<Button
+                variant="outlined"
+                fullWidth
+                color="primary"
+                onClick={() => changePage({ locationSearch: locationSearch,
+                                            path: `/${PAGES.BOOK_EDIT}` })}
+                > {formatMessage({
+                      id: 'book.create',
+                  })} </Button>)}
+            </Grid>
+        </Grid>
+
+        <Grid item container xs={12}>
+        { (books.isFetching || authors.isFetching) &&
+            (
+                <Grid container justify="space-around" >
+                     <CircularProgress />
+                </Grid>
+            )
+        }
+        </Grid>
+
+        <Grid item container xs={12}>
+        { (books.isFailed || authors.isFailed) &&
+          !(books.isFetching || authors.isFetching) &&
+            (
+                <Grid container justify="space-around" >
+                    <Typography variant="h5">
+                        {formatMessage({id: "data.loading.error"})}
+                    </Typography>
+                </Grid>
+            )
+        }
+        </Grid>
+
+        <Grid item container>
+        { Object.keys(authorsMap).length !==0 &&
+            (<Grid container justify="space-around" spacing={2} xs={12} >
+                {
+                    books.list.map((item)=>{
+                        const author = authorsMap[Number(item.authorId)];
+                        return (
+                            <Grid item >
+                                <BookCard book={item} author={author}
+                                    onEdit={() => changePage({ path: `/${PAGES.BOOK_EDIT}/${item.id}` })}
+                                    onDelete={ () => dispatch(removeBookFromPage({id: item.id}))}
+                                    canEditCard={canEditList}
+                                />
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>)
+        }
+        </Grid>
+
+    </Grid>
     )
 };
 
